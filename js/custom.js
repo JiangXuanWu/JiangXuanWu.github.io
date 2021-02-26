fc342abce504b140da7240801b92327e0c76c46c
JiangXuanWu <JiangXuanWu@outlook.com>
2021-02-26 17:42:44 +0800

    rebuilding site 2021年02月26日 17:42:44

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 /* 轮播背景图片 */
 $(function () {
 	$.backstretch([
-		  "/images/background/Observatory.jpg",
+		  "https://img.imgdb.cn/item/600a9c763ffa7d37b3235dae.png",
 		  "/images/background/Smoking1.jpg",
 		  "/images/background/Smoking2.png"
 	], { duration: 60000, fade: 1500 });
