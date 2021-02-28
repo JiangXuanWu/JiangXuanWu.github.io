9db7c00f17f87c4858bd903a49cdbce4b74beba8
JiangXuanWu <JiangXuanWu@outlook.com>
2021-02-28 17:57:54 +0800

    rebuilding site 2021年02月28日 17:57:54

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,7 @@ $(function () {
 	$.backstretch([
         "https://cdn.jsdelivr.net/gh/JiangXuanWu/DeployBlog@main/images/background/share1.jpg",
         "https://cdn.jsdelivr.net/gh/JiangXuanWu/DeployBlog@main/images/background/share2.jpg",
+        "https://cdn.jsdelivr.net/gh/JiangXuanWu/DeployBlog@main/images/background/share3.jpg",
         "https://cdn.jsdelivr.net/gh/JiangXuanWu/DeployBlog@main/images/background/Far.jpg"
 	], { duration: 60000, fade: 150 });
 });
