e91f4ae25276edc3b7bdf7ab48a366f92d665624
JiangXuanWu <JiangXuanWu@outlook.com>
2021-02-28 17:11:51 +0800

    rebuilding site 2021年02月28日 17:11:51

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,10 @@
 /* 轮播背景图片 */
 $(function () {
 	$.backstretch([
-		  "https://img.imgdb.cn/item/600a9c763ffa7d37b3235dae.png",
-		  "/images/background/Smoking1.jpg",
-		  "/images/background/Smoking2.png"
-	], { duration: 60000, fade: 1500 });
+        // "https://cdn.jsdelivr.net/gh/JiangXuanWu/BlogDeploy.github.io@master/images/background/Far.jpg"
+        //https://cdn.jsdelivr.net/gh/lewky/lewky.github.io@master/images/background/saber2.jpg
+        "https://img.imgdb.cn/item/603b58dc5f4313ce25a87aa6.jpg",
+        "https://img.imgdb.cn/item/6038bfcb5f4313ce25260416.jpg",
+        "https://img.imgdb.cn/item/600a9c763ffa7d37b3235dae.png"
+	], { duration: 60000, fade: 150 });
 });
