542cfc146c8836a38c58d37454b8da7e94da0bc5
JiangXuanWu <JiangXuanWu@outlook.com>
2021-03-02 15:15:37 +0800

    rebuilding site 2021年03月 2日 15:15:37

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
 /* 轮播背景图片 */
 $(function () {
 	$.backstretch([
-        "https://cdn.jsdelivr.net/gh/JiangXuanWu/DeployBlog@main/images/background/share1.jpg",
-        "https://cdn.jsdelivr.net/gh/JiangXuanWu/DeployBlog@main/images/background/share2.jpg",
-        "https://cdn.jsdelivr.net/gh/JiangXuanWu/DeployBlog@main/images/background/share3.jpg",
-        "https://cdn.jsdelivr.net/gh/JiangXuanWu/DeployBlog@main/images/background/Far.jpg"
+        "https://cdn.jsdelivr.net/gh/JiangXuanWu/JiangXuanWu.github.io@main/static/background/share1.jpg",
+        "https://cdn.jsdelivr.net/gh/JiangXuanWu/JiangXuanWu.github.io@main/static/background/share2.jpg",
+        "https://cdn.jsdelivr.net/gh/JiangXuanWu/JiangXuanWu.github.io@main/static/background/share3.jpg",
+        "https://cdn.jsdelivr.net/gh/JiangXuanWu/JiangXuanWu.github.io@main/static/background/Far.jpg"
 	], { duration: 60000, fade: 150 });
 });
