f21ebceebf77067fb8a495db8b16b0a955ed9a76
JiangXuanWu <jiangxuan.wu@outlook.com>
2021-05-14 11:56:55 +0800

    1

--- js/custom.js
+++
@@ -1,8 +0,0 @@
-/* 轮播背景图片 */
-$(function () {
-	$.backstretch([
-        "https://cdn.jsdelivr.net/gh/JiangXuanWu/JiangXuanWu.github.io@main/static/background/boy.jpg",
-        "https://cdn.jsdelivr.net/gh/JiangXuanWu/JiangXuanWu.github.io@main/static/background/clouds.jpg",
-        "https://cdn.jsdelivr.net/gh/JiangXuanWu/JiangXuanWu.github.io@main/static/background/water.jpg",
-	], { duration: 60000, fade: 150 });
-});
